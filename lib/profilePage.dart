eb0703615f8d6526881d3f36b63bbd188b882eae
JIANWEI <u1172348@utah.edu>
2023-04-25 12:49:52 -0600

    Comment out some errorr codes.

--- lib/profilePage.dart
+++ lib/profilePage.dart
@@ -1,34 +1,31 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:roommates/LoginPage.dart';
 import 'package:roommates/settingsPage.dart';
 
 class ProfilePage extends StatefulWidget {
-
   ProfilePage({Key? key}) : super(key: key);
-
   @override
   State<ProfilePage> createState() => _profilePage();
 }
 class _profilePage extends State<ProfilePage> {
-  late String userName;
-  late String email;
-  late String password;
+  // late String userName;
+  // late String email;
+  // late String password;
 
   Future<void> _signOut() async {
     await FirebaseAuth.instance.signOut();
     Navigator.of(context).pushReplacement(
         MaterialPageRoute(builder: ((context) => LoginPage())));
   }
-  void getUserData() async {
-    final user = FirebaseAuth.instance.currentUser;
-    DocumentSnapshot db =  await FirebaseFirestore.instance.collection("Users").doc(user?.uid).get();
-     userName = db.get("UserName");
-     email = db.get("Email");
-     password = db.get("Password");
-  }
+  // void getUserData() async {
+  //   final user = FirebaseAuth.instance.currentUser;
+  //   DocumentSnapshot db =  await FirebaseFirestore.instance.collection("Users").doc(user?.uid).get();
+  //    userName = db.get("UserName");
+  //    email = db.get("Email");
+  //    password = db.get("Password");
+  // }
 
   static const TextStyle optionStyle =
   TextStyle(fontSize: 30, fontWeight: FontWeight.w600);
@@ -160,7 +157,7 @@ class _profilePage extends State<ProfilePage> {
   }
 
   Widget buildUserInformation(String label, String text)  {
-    getUserData();
+   //s getUserData();
     return Padding(
       padding: EdgeInsets.only(bottom: 30),
       child: TextField(
