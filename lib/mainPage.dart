5e9f0193ae4f256e0ed3a5739e27fae4e79a700f
JIANWEI0822 <u1172348@utah.edu>
2023-11-19 21:48:54 -0700

    Merge code to the final, and fix the problem. Everyone should check your own features to see if any problems.

--- lib/mainPage.dart
+++ lib/mainPage.dart
@@ -8,26 +8,24 @@ import 'package:roommates/homePage/homePage.dart';
 import 'package:roommates/Notifications/NotificationView.dart';
 import 'package:roommates/profilePage.dart';
 import 'package:roommates/themeData.dart';
+import 'api/firebase_api.dart';
 import 'calendarPage/calendarPage.dart';
 
-import 'package:roommates/api/firebase_api.dart';
 
 class mainPage extends StatefulWidget {
-
   final int navigateToScreen;
-
   const mainPage({Key? key, required this.navigateToScreen}) : super(key: key);
 
+
   @override
-  State<mainPage> createState() => _mainPageState();
+  _mainPageState createState() => _mainPageState();
+
 
 }
 
 class _mainPageState extends State<mainPage> {
   int _selectedIndex = 0;
-  String themeBrightness = "";
-  String color = "";
-
+  String? currUser = FirebaseAuth.instance.currentUser?.uid;
   static const TextStyle optionStyle =
   TextStyle(fontSize: 30, fontWeight: FontWeight.w600);
   final screens = [
@@ -36,10 +34,9 @@ class _mainPageState extends State<mainPage> {
     CostSplitView(),
     NotificationView(),
     ProfilePage(),
+
   ];
-
   late int screenCopy;
-
   FirebaseApi pushNotifCon = FirebaseApi();
 
   @override
@@ -47,31 +44,9 @@ class _mainPageState extends State<mainPage> {
     super.initState();
     screenCopy = widget.navigateToScreen;
     _selectedIndex = screenCopy;
-    // print("Sent from mainPage.dart");
-
-    // Initialize the entire notification stuff upon start up of the app.
-    // Sends the token to firebase when the app itself is building up.
-    // Will send regardless of if person is not signed in, or already is since main page needs to build.
     pushNotifCon.initNotifications();
   }
 
-  Future getUserData() async {
-    String? user = FirebaseAuth.instance.currentUser?.uid;
-    if (user != null) {
-      DocumentSnapshot db = await FirebaseFirestore.instance.collection("Users")
-          .doc(user)
-          .get();
-      Map<String, dynamic> list = db.data() as Map<String, dynamic>;
-      if (mounted) {
-        setState(() {
-          color = list['themeColor'];
-          themeBrightness = list['themeBrightness'];
-        });
-      }
-    }
-
-
-  }
   String returnPageTitle(int index) {
     String title = "";
     switch (index) {
@@ -116,106 +91,101 @@ class _mainPageState extends State<mainPage> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: Future.wait([]),
-    builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-    if (snapshot.hasData) {
-    return StreamBuilder(
-    stream: FirebaseFirestore.instance
-        .collection('Users')
-        .doc(currUser!)
-        .snapshots(),
-    builder: (context, snapshot) {
-      // If there's an error.
-      if (snapshot.hasError) {
-        return Text('Something went wrong! ${snapshot.data}');
-      }
-      // If there's no error and the snapshot has data.
-      else if (snapshot.hasData) {
-        // Setting the task data.
-        final UserData = snapshot.data!;
-        return Scaffold(
-          backgroundColor: setBackGroundBarColor(UserData['themeBrightness']),
-          body: screens[_selectedIndex],
-          bottomNavigationBar: Container(
-            decoration: BoxDecoration(
-              color: setAppBarColor(UserData['themeColor'], UserData['themeBrightness']),
-              boxShadow: [
-                BoxShadow(
-                  blurRadius: 20,
-                  color: Colors.black.withOpacity(.1),
-                )
-              ],
-            ),
-            child: SafeArea(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 15.0, vertical: 8),
-                child: GNav(
-                  rippleColor: transparent(UserData['themeColor'], UserData['themeBrightness']),
-                  hoverColor: transparent(UserData['themeColor'], UserData['themeBrightness']),
-                  gap: 3,
-                  activeColor: setBackGroundBarColor( UserData['themeBrightness']),
-                  iconSize: 25,
-                  padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-                  duration: const Duration(milliseconds: 400),
-                  tabBackgroundColor: deep(UserData['themeColor'],  UserData['themeBrightness']),
-                  color: setBackGroundBarColor( UserData['themeBrightness']),
-                  tabs: const [
-                    GButton(
-                      icon: Icons.home,
-                      text: "Home",
+      future: Future.wait([]),
+      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+        if (snapshot.hasData) {
+          return StreamBuilder(
+              stream: FirebaseFirestore.instance
+                  .collection('Users')
+                  .doc(currUser!)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                // If there's an error.
+                if (snapshot.hasError) {
+                  return Text('Something went wrong! ${snapshot.data}');
+                }
+                // If there's no error and the snapshot has data.
+                else if (snapshot.hasData) {
+                  // Setting the task data.
+                  final UserData = snapshot.data!;
+                  return Scaffold(
+                    backgroundColor: setBackGroundBarColor(UserData['themeBrightness']),
+                    body: screens[_selectedIndex],
+                    bottomNavigationBar: Container(
+                      decoration: BoxDecoration(
+                        color: setAppBarColor(UserData['themeColor'], UserData['themeBrightness']),
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 20,
+                            color: Colors.black.withOpacity(.1),
+                          )
+                        ],
+                      ),
+                      child: SafeArea(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 15.0, vertical: 8),
+                          child: GNav(
+                            rippleColor: transparent(UserData['themeColor'], UserData['themeBrightness']),
+                            hoverColor: transparent(UserData['themeColor'], UserData['themeBrightness']),
+                            gap: 3,
+                            activeColor: setBackGroundBarColor( UserData['themeBrightness']),
+                            iconSize: 25,
+                            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                            duration: const Duration(milliseconds: 400),
+                            tabBackgroundColor: deep(UserData['themeColor'],  UserData['themeBrightness']),
+                            color: setBackGroundBarColor( UserData['themeBrightness']),
+                            tabs: const [
+                              GButton(
+                                icon: Icons.home,
+                                text: "Home",
+                              ),
+                              GButton(
+                                icon: Icons.calendar_month,
+                                text: "Calendar",
+                              ),
+                              GButton(
+                                icon: Icons.money,
+                                text: "Split Pay",
+                              ),
+                              GButton(
+                                icon: Icons.notifications,
+                                text: "Notification",
+                              ),
+                              GButton(
+                                icon: Icons.account_circle,
+                                text: "Profile",
+                              ),
+                            ],
+                            selectedIndex: _selectedIndex,
+                            onTabChange: (index) {
+                              setState(() {
+                                _selectedIndex = index;
+                              });
+                            },
+                          ),
+                        ),
+                      ),
                     ),
-                    GButton(
-                      icon: Icons.calendar_month,
-                      text: "Calendar",
-                    ),
-                    GButton(
-                      icon: Icons.money,
-                      text: "Split Pay",
-                    ),
-                    GButton(
-                      icon: Icons.notifications,
-                      text: "Notification",
-                    ),
-                    GButton(
-                      icon: Icons.account_circle,
-                      text: "Profile",
-                    ),
-                  ],
-                  selectedIndex: _selectedIndex,
-                  onTabChange: (index) {
-                    setState(() {
-                      _selectedIndex = index;
-                    });
-                  },
-                ),
-                GButton(
-                  icon: Icons.calendar_month,
-                  text: "Calendar",
-                ),
-                GButton(
-                  icon: Icons.money,
-                  text: "Split Pay",
-                ),
-                GButton(
-                  icon: Icons.notifications,
-                  text: "Notification",
-                ),
-                GButton(
-                  icon: Icons.account_circle,
-                  text: "Profile",
-                ),
-              ],
-              selectedIndex: _selectedIndex,
-              onTabChange: (index) {
-                setState(() {
-                  _selectedIndex = index;
-                });
-              },
-            ),
-          ),
-        ),
-      ),
+                  );
+                }
+                else {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              }
+          );
+        }
+        else if (snapshot.hasError) {
+          return Text("Something went wrong! ${snapshot.error}");
+        }
+        else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
     );
   }
 }
